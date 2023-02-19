c4e0a6b1852f2e36a1394e8a7d6a77f80680a5ff
pradeep_kumar <Pradeep_Kumar3@epam.com>
2023-02-19 23:48:45 +0530

    initial commit

---
+++ InventoryService/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace InventoryService.Models
+{
+    public partial class Products
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int AvailableQuantity { get; set; }
+    }
+}
